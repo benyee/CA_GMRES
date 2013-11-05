f45ac8b75507e7ffd56d1ca033088e6fa402fa26
Ben <benyee@berkeley.edu>
2013-11-05 15:48:41 -0500

    Created SparseVec class
    Can now read in sparse vectors and print them out.  Also, in the last
    commit, I made a SpraseMat class with similar capabilities.

---
+++ SparseVec.cpp
@@ -0,0 +1,44 @@
+//
+//  SparseVec.cpp
+//  
+//
+//  Created by Ben Yee on 11/5/13.
+//
+//
+
+#include "SparseVec.h"
+
+SparseVec::SparseVec(){
+}
+
+void SparseVec::readFullVector(string inputfile, char delim){
+    ifstream inputFile;
+    inputFile.open(inputfile.c_str());
+    
+    string line;
+    
+    unsigned int row = 0;
+    while(getline(inputFile,line)){ //while not at the end of the file
+        double value = atof(line.c_str());
+        if (value != 0){
+            b.push_back(value);
+            Ib.push_back(row);
+        }
+        row++;
+    }
+    Ib.push_back(b.size());
+}
+
+void SparseVec::print_vector(){
+    cout<<"b = [";
+    for(unsigned int i = 0; i<b.size(); i++){
+        cout<<b[i]<<" ";
+    }
+    cout<<"]"<<endl;
+    
+    cout<<"Ib = [";
+    for(unsigned int i = 0; i<Ib.size(); i++){
+        cout<<Ib[i]<<" ";
+    }
+    cout<<"]"<<endl;
+}
